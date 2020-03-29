44c97283fba77d6f7d406021d32eb25572b86fc0
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-29 00:17:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,8 +10,8 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   the_manifest = {
     "whale bone corsets" => "5",
-
-
+    "porcelain vases" => "2",
+    "oil paintings" => "3"
   }
 
 end
